c16219ce68bf22bb54cf3ce5e0ee4ed71c3b2935
Naveen <59356289+naveenr-btc@users.noreply.github.com>
2021-11-17 14:33:19 +0530

    Update VerticalRangeSeekBar.java

--- RangeSeekBar/src/main/java/com/jaygoo/widget/VerticalRangeSeekBar.java
+++ RangeSeekBar/src/main/java/com/jaygoo/widget/VerticalRangeSeekBar.java
@@ -137,6 +137,7 @@ public class VerticalRangeSeekBar extends RangeSeekBar {
                 final String text2Draw = getTickMarkTextArray()[i].toString();
                 if (TextUtils.isEmpty(text2Draw)) continue;
                 paint.getTextBounds(text2Draw, 0, text2Draw.length(), tickMarkTextRect);
+                paint.setTextAlign(Paint.Align.LEFT);
                 paint.setColor(getTickMarkTextColor());
                 //平分显示
                 float x;
